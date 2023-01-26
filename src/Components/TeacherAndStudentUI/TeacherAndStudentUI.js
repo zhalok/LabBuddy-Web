5a73caff6fd0deccefd6987bcaf876f243f35bd3
IT Gadget <admin@ITs-MacBook-Pro.local>
2023-01-27 00:37:30 +0600

    added akash-task

--- src/Components/TeacherAndStudentUI/TeacherAndStudentUI.js
+++ src/Components/TeacherAndStudentUI/TeacherAndStudentUI.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import s1 from '../../Image/ss.png'
+import p1 from '../../Image/teaching.png'
 
 export default function TeacherAndStudentUI() {
   return (
-    <div className="bg-TAS">
+    <div className="bg-TAS d-flex ">
+        <div className="my-auto mx-auto">
+            <div className="d-flex ">
+            <div className="bg-theme shadow-lg p-4 me-4">
+                <img src={s1} alt="" width="140px" height="120px"/>
+                <h4 className="text-light fw-bold my-3">Students</h4>
+            </div>
+            <div className="bg-theme shadow-lg p-4">
+                <img src={p1} alt="" width="140px" height="120px"/>
+                <h4 className="text-light fw-bold my-3">Teacher</h4>
+            </div>
+            </div>
+
+        </div>
       
     </div>
   )
